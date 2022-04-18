0f61b4832030d68b1e46ab2623f9a6eea263f88f
Carlos Gerardo Martin <cm43833@gmail.com>
2022-04-18 10:29:33 +0100

    Added admin interface and inital javascript

---
+++ public/admin/Admin.js
@@ -0,0 +1,44 @@
+class Admin (){
+
+  constructor (username, password, telephoneNumber){
+      this.username = username;
+      this.password = password;
+      this.telephoneNumber = telephoneNumber;
+  }
+
+  resetSystem()
+  {
+
+  }
+
+  setMoney(user, double)
+  {
+
+  }
+
+  addCarPark(xNum, yNum, carPark)
+  {
+
+  }
+  
+  edit carPark(,car)
+  {
+		
+  }
+
+  blockSpace(parkingSpace)
+  {
+
+  }
+
+  unblockSpace(parkingSpace)
+  {
+
+  }
+
+  banUser(user)
+  {
+
+  }
+
+}
